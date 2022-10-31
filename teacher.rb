b070e351fb476b2e9180f0298b8c4e8ccab85399
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-10-31 16:31:06 +0100

    Add Teacher class and Constructor

---
+++ teacher.rb
@@ -0,0 +1,10 @@
+class Teacher < Person
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    @specialization = specialization
+    super(age, name, parent_permission)
+  end
+
+  def can_use_services?
+    true
+  end
+end
